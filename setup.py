12db26991abf7a3751af64116bb9fca30d404361
Andrew Houghton <andrew.houghton@mediushealth.org>
2019-12-23 08:04:32 +1100

    Uploaded to pypi

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from distutils.core import setup
 
 setup(
     name="appear",
-    version="1.0",
+    version="1.1",
     description="Make program internals appear in the browser",
     author="Andrew Houghton",
     author_email="houghtonandrew0@gmail.com",
     url="https://github.com/andrew-houghton/appear",
     packages=["appear"],
-    long_description=open('README.md', 'r').read(),
     install_requires=['Flask-SocketIO', 'grequests'],
 )
