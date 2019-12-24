ab76fbd5c5af8a7b486acc61afa18c21e7b2a661
Andrew Houghton <andrew.houghton@mediushealth.org>
2019-12-24 11:39:41 +1100

    Change to use correct setup tools to fix PyPi markdown

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from pathlib import Path
 
 
@@ -8,7 +8,7 @@ readme_content = open(Path(__file__).parent.joinpath("README.md"), "r").read()
 
 setup(
     name="appear",
-    version="1.2",
+    version="1.2.2",
     description="Make program internals appear in the browser",
     author="Andrew Houghton",
     author_email="houghtonandrew0@gmail.com",
@@ -18,5 +18,5 @@ setup(
     include_package_data=True,
     install_requires=["Flask-SocketIO", "grequests"],
     long_description=readme_content,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown",
 )
